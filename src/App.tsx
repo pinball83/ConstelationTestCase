b042e548365320d9aa5786f1c61b117356cf0fcd
pinball83 <raggamuffin.inda.shell@gmail.com>
2025-06-30 15:33:04 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import {StatusBar, StyleSheet, useColorScheme, View} from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
