b963bcbe366d2d5e496ebf6307a0d3d3e0418cfb
pinball83 <raggamuffin.inda.shell@gmail.com>
2025-06-30 15:33:32 +0100

    Add auction model

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export interface Vehicle {
+    id: number;
+    make: string;
+    model: string;
+    engineSize: string;
+    fuelType: string;
+    year: number;
+    mileage: number;
+    auctionDateTime: string;
+    startingBid: number;
+    image: string;
+}
