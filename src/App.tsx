a0995e12f8a57511c70b8dde7b4cc8bc3ebde83e
pinball83 <raggamuffin.inda.shell@gmail.com>
2025-07-02 01:25:41 +0100

    Refactor App component; clean up import statements and formatting for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { NavigationWrapper } from "./navigation/NavigationWrapper.tsx";
-import {ThemeProvider} from "./theme/ThemeContext.tsx";
-
+import { NavigationWrapper } from './navigation/NavigationWrapper.tsx';
+import { ThemeProvider } from './theme/ThemeContext.tsx';
 
 //todo: add details screen
 //todo: add filtering
+//todo: add zustand for state management and persist favorite vehicles
 function App() {
-
-    return (
-        <ThemeProvider>
-            <NavigationWrapper />
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider>
+      <NavigationWrapper />
+    </ThemeProvider>
+  );
 }
 
 export default App;
